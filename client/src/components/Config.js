1b527488bb27c8819403cf138a9e01963013ac45
JM261 <wlaks0516@gmail.com>
2022-04-22 20:52:29 +0900

    3rd commit

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,6 +2,6 @@
 export const USER_SERVER = '/api/users';
 
 export const API_URL = 'http://api.themoviedb.org/3/';
-
+export const API_KEY = '65732536ad273de60146723aab1daa18';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
 
