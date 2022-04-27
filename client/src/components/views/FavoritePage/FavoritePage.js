27a5074ce9db5ccc05091bb568481cf2d2e39769
JM261 <wlaks0516@gmail.com>
2022-04-28 08:54:10 +0900

    9th commit feat : favorite list_hoverImage & remove_Favorite

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './favorite.css';
 import Axios from 'axios';
+import { Popover } from 'antd';
+import { IMAGE_BASE_URL } from '../../Config';
 
 function FavoritePage() {
 
@@ -9,6 +11,11 @@ function FavoritePage() {
 
   useEffect(() => {
 
+      fetchFavoredMovie()
+
+  }, [])
+
+  const fetchFavoredMovie = () => {
     Axios.post('/api/favorite/getFavoredMovie', { userFrom: localStorage.getItem('userId') })
         .then(response => {
             if (response.data.success) {
@@ -18,8 +25,49 @@ function FavoritePage() {
                 alert('영화 정보를 가져오는데 실패했습니다.')
             }
         })
+  }
 
-  }, [])
+  const onClickDelete = (movieId, userFrom) => {
+
+      const variables = {
+        movieId,
+        userFrom
+      }
+
+      Axios.post('/api/favorite/removeFromFavortie', variables)
+        .then(response => {
+            if(response.data.success) {
+                fetchFavoredMovie()
+            } else {
+                alert("리스트에서 삭제하는데 실패했습니다.")
+            }
+        })
+  }
+
+
+  const renderCards = Favorites.map((favorite, index) => {
+
+      const content = (
+          <div>
+              {favorite.moviePost ?
+              
+                  <img src={`${IMAGE_BASE_URL}w500${favorite.moviePost}`} /> : "no Image"
+          }
+          </div>
+      )
+
+      return <tr key={index}>
+
+          <Popover content={content} title={`${favorite.movieTitle}`} >
+              <td>{favorite.movieTitle}</td>
+          </Popover>
+
+          <td>{favorite.movieRunTime} mins</td>
+          <td><button onClick={() => onClickDelete(favorite.movieId, favorite.userFrom)}>Remove</button></td>
+
+      </tr>
+})
+
 
   return (
     <div style={{ width: '85%', margin: '3rem auto' }}>
@@ -35,15 +83,10 @@ function FavoritePage() {
               </tr>
           </thead>
           <tbody>
-            {Favorites.map((favorite, index) => (
-                <tr key={index}>
-                    <td>{favorite.movieTitle}</td>
-                    <td>{favorite.movieRunTime} mins</td>
-                    <td><button>Remove</button></td>
 
-                </tr>
 
-            ))}
+            {renderCards}
+
 
           </tbody>
         </table>
